36af49d087489f684e54a744e4381c04e09a0e22
Dipti Kumari <diptiku2002@gmail.com>
2024-03-06 19:21:49 +0530

    folder structure added

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import "./index.css";
+function App() {
+  return (
+    <h1 className="text-3xl text-blue-950 font-bold underline">CredLock</h1>
+  );
+}
+
+export default App;
