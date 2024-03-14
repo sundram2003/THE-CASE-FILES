d6e8165fb5fd6f5f46b4d59a003095c8339dc01b
sundram2003 <sundramm2003@gmail.com>
2024-03-14 16:41:07 +0530

    follow and unfollow routes tested

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,23 +4,32 @@ import { User } from "../models/user.model.js";
 export const followUser = async (req, res) => {
   try {
     const loggedInUser = req.user.id;
-    const { userToFollow } = req.body;
+    const { username } = req.body;
     const loggedInUserDetails = await User.findById(loggedInUser);
-    const userToFollowDetails = await User.findById(userToFollow);
+    const userToFollowDetails = await User.findOne({ username });
+    console.log("loggedINUserDetails: ", loggedInUserDetails);
+    console.log("userTofollowDetails: ", userToFollowDetails)
     if (!userToFollowDetails || userToFollowDetails === null || userToFollowDetails === undefined) {
       return res.status(404).json({
         success: false,
         message: "User not found"
       });
     }
-    if (loggedInUser.following.includes(userToFollow)) {
+    if (loggedInUser == userToFollowDetails._id) {
+      return res.status(400).json({
+        success: false,
+        message: "User Cannot follow himself",
+
+      })
+    }
+    if (loggedInUserDetails.following.includes(userToFollowDetails._id)) {
       return res.status(403).json({
         sucess: false,
         message: "Already following the user"
       });
     }
-    loggedInUserDetails.following.push(userId);
-    userToFollowDetails.followers.push(req.userId);
+    loggedInUserDetails.following.push(userToFollowDetails._id);
+    userToFollowDetails.followers.push(loggedInUser);
     await loggedInUserDetails.save();
     await userToFollowDetails.save();
     res.status(200).json({
@@ -30,29 +39,33 @@ export const followUser = async (req, res) => {
   }
   catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error
+    });
   }
 }
 export const unfollowUser = async (req, res) => {
   try {
     const loggedInUser = req.user.id;
-    const { userToUnfollow } = req.body;
+    const { username } = req.body;
     const loggedInUserDetails = await User.findById(loggedInUser);
-    const userToUnfollowDetails = await User.findById(userToUnfollow);
+    const userToUnfollowDetails = await User.findOne({ username });
     if (!userToUnfollowDetails || userToUnfollowDetails === null || userToUnfollowDetails === undefined) {
       return res.status(404).json({
         success: false,
         message: "User not found"
       });
     }
-    if (!loggedInUserDetails.following.includes(userToUnfollow)) {
+    if (!loggedInUserDetails.following.includes(userToUnfollowDetails._id)) {
       return res.status(403).json({
         sucess: false,
         message: "Not following the user"
       });
     }
-    loggedInUserDetails.following.pull(userToUnfollow);
-    userToUnfollowDetails.followers.pull(loggedInUser);
+    loggedInUserDetails.following.pull(userToUnfollowDetails._id);
+    userToUnfollowDetails.followers.pull(loggedInUserDetails._id);
     await loggedInUserDetails.save();
     await userToUnfollowDetails.save();
     res.status(200).json({
@@ -62,7 +75,11 @@ export const unfollowUser = async (req, res) => {
   }
   catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error,
+    });
   }
 }
 export const getUserByUserName = async (req, res) => {
